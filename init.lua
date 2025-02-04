da7e433900cabf4b60f9591b9a794f13a925b9ad
Eric Wong <wsdjeg@outlook.com>
2025-02-04 14:52:47 +0800

    update

--- init.lua
+++ init.lua
@@ -5,13 +5,28 @@
 -- License: GPLv3
 --=============================================================================
 
+require('plug').setup({
+
+  bundle_dir = 'D:\\bundle_dir\\',
+
+})
+
+require('autocmds')
+require('options')
+require('keymaps')
+
 require("plug").add({
 	{
 		"wsdjeg/scrollbar.vim",
 		events = { "VimEnter" },
+    config = function()
+    end
 	},
 	{
 		"wsdjeg/flygrep.nvim",
 		cmds = { "FlyGrep" },
+    config = function()
+      require('flygrep').setup()
+    end
 	},
 })
