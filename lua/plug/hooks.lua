7444a14f0a710b8c523d968a835457bec7fb54e1
Eric Wong <wsdjeg@outlook.com>
2025-02-04 15:46:33 +0800

    support on cmd

--- lua/plug/hooks.lua
+++ lua/plug/hooks.lua
@@ -5,6 +5,7 @@ local group = vim.api.nvim_create_augroup("plugin_hooks", { clear = true })
 local plugin_loader = require("plug.loader")
 
 local event_plugins = {}
+local cmd_plugins = {}
 
 function M.on_events(events, plugSpec)
 	event_plugins[plugSpec.name] = vim.api.nvim_create_autocmd(events, {
@@ -17,11 +18,21 @@ function M.on_events(events, plugSpec)
 	})
 end
 
+--- @param cmds table<string>
+--- @param plugSpec PluginSpec
 function M.on_cmds(cmds, plugSpec)
-
-  for _, cmd in ipairs(cmds) do
-  end
-  
+	for _, cmd in ipairs(cmds) do
+    cmd_plugins[cmd] = plugSpec
+		vim.api.nvim_create_user_command(cmd, function(opt)
+      plugin_loader.load(cmd_plugins[opt.name])
+      vim.cmd(opt.name .. ' ' .. opt.args)
+    end, {
+			nargs = "*",
+			complete = function(...)
+				return {}
+			end,
+		})
+	end
 end
 
 return M
