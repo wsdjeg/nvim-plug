df62a42d7848538eca50bf3584f3b66265a3a988
Eric Wong <wsdjeg@outlook.com>
2025-02-04 14:39:10 +0800

    add installer

---
+++ lua/plug/installer.lua
@@ -0,0 +1,15 @@
+--=============================================================================
+-- installer.lua
+-- Copyright 2025 Eric Wong
+-- Author: Eric Wong < wsdjeg@outlook.com >
+-- License: GPLv3
+--=============================================================================
+
+local M = {}
+
+
+M.install = function(plugSpec)
+end
+
+
+return M
--- lua/plug/loader.lua
+++ lua/plug/loader.lua
@@ -8,8 +8,30 @@
 
 local M = {}
 
+-- {'loadconf': 1,
+-- 'type': 'none',
+-- 'overwrite': 1,
+-- 'lazy': 0,
+-- 'name': 'defx-git',
+-- 'rtp': 'C:/Users/wsdjeg/.SpaceVim/bundle/defx-git',
+-- 'normalized_name': 'defx-git',
+-- 'local': 1,
+-- 'sourced': 1,
+-- 'orig_opts': {'repo': 'C:/Users/wsdjeg/.SpaceVim/bundle/defx-git',
+-- 'loadconf': 1,
+-- 'type': 'none',
+-- 'merged': 0,
+-- 'hook_source': 'call SpaceVim#util#loadConfig(''plugins/defx-git.vim'')',
+-- 'overwrite': 1},
+-- 'repo': 'C:/Users/wsdjeg/.SpaceVim/bundle/defx-git',
+-- 'hook_source': 'call SpaceVim#util#loadConfig(''plugins/defx-git.vim'')',
+-- 'called': {'''call SpaceVim#util#loadConfig(''''plugins/defx-git.vim'''')''': v:true},
+-- 'merged': 0,
+-- 'path': 'C:/Users/wsdjeg/.SpaceVim/bundle/defx-git'}
 function M.load(plugSpec)
-  
+  if plugSpec.rtp then
+    vim.opt.runtimepath:append(plugSpec.rtp)
+  end
 end
 
 
