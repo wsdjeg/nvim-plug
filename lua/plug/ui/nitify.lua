ae916f0cca0a4580bbd6f692474d9aaaa48a47f8
Eric Wong <wsdjeg@outlook.com>
2025-03-19 11:09:08 +0800

    fix function name

--- lua/plug/ui/nitify.lua
+++ lua/plug/ui/nitify.lua
@@ -2,7 +2,7 @@ local notify = require('notify')
 
 local M = {}
 
-function M.on_uiupdate(name, data)
+function M.on_uidate(name, data)
   if data.build_done then
     notify.notify(string.format('%s build done', name))
   elseif data.curl_done or data.clone_done then
