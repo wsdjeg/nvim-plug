f894a3b7cda80c2ae989d5c1a6f75e46aec65926
Eric Wong <wsdjeg@outlook.com>
2025-10-13 22:31:33 +0800

    feat: add actions for picker source

--- lua/picker/sources/plug.lua
+++ lua/picker/sources/plug.lua
@@ -2,31 +2,42 @@ local M = {}
 local plug = require('plug')
 local terminal = require('terminal')
 
-local previewer = require("picker.previewer.file")
+local previewer = require('picker.previewer.file')
 
 function M.get()
-    local p = {}
-    local plugins = plug.get()
+  local p = {}
+  local plugins = plug.get()
 
-    for _, k in pairs(plugins) do
-        table.insert(p, k)
-    end
-    return vim.tbl_map(function(t)
-        return { value = t, str = t.name }
-    end, p)
+  for _, k in pairs(plugins) do
+    table.insert(p, k)
+  end
+  return vim.tbl_map(function(t)
+    return { value = t, str = t.name }
+  end, p)
+end
+
+function M.actions()
+  return {
+    ['<Enter>'] = M.default_action,
+    ['<C-b>'] = M._open_url,
+  }
 end
 
 function M.default_action(entry)
-    terminal.open(entry.value.path)
-    -- make sure it is terminal mode
-    vim.cmd('noautocmd startinsert')
+  terminal.open(entry.value.path)
+  -- make sure it is terminal mode
+  vim.cmd('noautocmd startinsert')
+end
+
+function M._open_url(entry)
+  vim.ui.open(entry.value.url)
 end
 
 M.preview_win = true
 
 ---@field item PickerItem
 function M.preview(item, win, buf)
-    previewer.preview(item.value.path .. '/README.md', win, buf)
+  previewer.preview(item.value.path .. '/README.md', win, buf)
 end
 
 return M
