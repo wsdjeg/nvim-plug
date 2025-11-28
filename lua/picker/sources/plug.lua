8baa3a745998ef2b7ae058386d4db1ac8e3ae846
Eric Wong <wsdjeg@outlook.com>
2025-11-28 22:44:26 +0800

    feat: use luarocks show for rocks preview

--- lua/picker/sources/plug.lua
+++ lua/picker/sources/plug.lua
@@ -2,6 +2,7 @@ local M = {}
 local plug = require('plug')
 
 local previewer = require('picker.previewer.file')
+local cmd_previewer = require('picker.previewer.cmd')
 
 function M.get()
     local p = {}
@@ -80,7 +81,15 @@ function M.preview(item, win, buf)
         previewer.preview(item.value.dev_path .. '/README.md', win, buf)
     elseif item.value.type == 'rocks' then
         if item.value.rtp then
-            previewer.preview(item.value.rtp .. '/README.md', win, buf)
+            if vim.fn.filereadable(item.value.rtp .. '/doc/README.md') == 1 then
+                previewer.preview(item.value.rtp .. '/doc/README.md', win, buf)
+            else
+                cmd_previewer.preview(
+                    { 'luarocks', 'show', item.value.name },
+                    win,
+                    buf
+                )
+            end
         end
     else
         previewer.preview(item.value.path .. '/README.md', win, buf)
